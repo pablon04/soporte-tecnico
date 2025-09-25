f3151b89f43a4e5e3c1639474fdb8d9e3a216e4f
Pablo Navarro <pablonavarrort@gmail.com>
2025-09-25 16:42:26 +0200

    proceso de autentificación, registro y ver base de datos, ch - falta log-in y mejora y pasarela a la ruta principal

--- src/app/auth/data-access/auth.service.ts
+++ src/app/auth/data-access/auth.service.ts
@@ -1,10 +1,29 @@
 import { inject, Injectable } from "@angular/core";
 import { SupabaseService } from "../../shared/data-access/supabase.service";
+import { SignUpWithPasswordCredentials } from "@supabase/supabase-js";
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
 
     private _supabaseClient = inject(SupabaseService).supabaseClient;
+
+    session() {
+
+    }
+
+    signUp(credentials: SignUpWithPasswordCredentials) {
+        return this._supabaseClient.auth.signUp(credentials);
+    }
+
+    logIn(credentials: SignUpWithPasswordCredentials) {
+        return this._supabaseClient.auth.signInWithPassword(credentials);
+    }
+
+    signOut() {
+        return this._supabaseClient.auth.signOut();
+    }
+
+
 
 
 }
--- src/app/auth/features/sign-up/sign-up.ts
+++ src/app/auth/features/sign-up/sign-up.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FormBuilder, Validators, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../data-access/auth.service';
+
+interface SignUpForm {
+  email: FormControl<null | string>;
+  password: FormControl<null | string>;
+}
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.css'
 })
 export default class SignUp {
+  
+  private _formBuilder = inject(FormBuilder);
+
+  private _authService = inject(AuthService);
+
+  form = this._formBuilder.group<SignUpForm>({
+    email: this._formBuilder.control(null, [Validators.required, Validators.email]),
+    password: this._formBuilder.control(null, [Validators.required]),
+
+  });
+
+ async submit() {
+    if (this.form.invalid) return;
+
+    try {
+      const authResponse = await this._authService.signUp({
+        email: this.form.value.email ?? '',
+        password: this.form.value.password ?? '',
+      });
+
+      if (authResponse.error) throw authResponse.error;
+
+      alert('Por favor revisa tu correo!');
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
 
 }
